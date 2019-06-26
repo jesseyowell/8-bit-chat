cd813943df560f03ce89352a65d9e92aa465a478
Jesse Yowell <jesse.yowell@gmail.com>
2019-06-26 12:45:46 -0700

    Updates

---
+++ precache-manifest.bd24d200ed573ef16c322c98bf0a6e64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4c1440c76c2a92a0646",
+    "url": "/8-bit-chat/static/css/main.747a2d53.chunk.css"
+  },
+  {
+    "revision": "c4c1440c76c2a92a0646",
+    "url": "/8-bit-chat/static/js/main.c4c1440c.chunk.js"
+  },
+  {
+    "revision": "586522c033284b903880",
+    "url": "/8-bit-chat/static/js/1.586522c0.chunk.js"
+  },
+  {
+    "revision": "899271edf43b860fdb15",
+    "url": "/8-bit-chat/static/js/runtime~main.899271ed.js"
+  },
+  {
+    "revision": "bf686b915dbed24207e43680d2b846bc",
+    "url": "/8-bit-chat/index.html"
+  }
+];
